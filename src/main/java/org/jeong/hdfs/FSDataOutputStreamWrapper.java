0699620abb7311146eee6ebdb27850d4f07456af
Jeonghoon2 <ukidd12@naver.com>
2024-01-12 04:54:10 +0900

    kafka에서 읽은 Json 형식의 데이터를 JAVA 객체로 변환하기 위한 메서드

--- src/main/java/org/jeong/hdfs/FSDataOutputStreamWrapper.java
+++ src/main/java/org/jeong/hdfs/FSDataOutputStreamWrapper.java
@@ -1,22 +1,30 @@
 package org.jeong.hdfs;
 
 import org.apache.hadoop.fs.FSDataOutputStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 public class FSDataOutputStreamWrapper {
 
     private Date streamOpenDate;
     private FSDataOutputStream out;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass().getName());
 
     public FSDataOutputStreamWrapper(FSDataOutputStream out) {
+        if (out == null) {
+            throw new IllegalArgumentException("Error FSDataOutputStream cannot be null");
+        }
         this.streamOpenDate = new Date();
         this.out = out;
     }
 
     public void write(String data) throws IOException {
-        out.write(data.getBytes());
+        logger.info("Write data information : {}",data.getBytes(StandardCharsets.UTF_8));
+        out.write(data.getBytes(StandardCharsets.UTF_8));
     }
 
     public void close() throws IOException {
